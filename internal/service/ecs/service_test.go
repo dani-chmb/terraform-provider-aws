cb15c7d83e053d514c5b0d64a433c3f521f6d223
Daniel Flores <daflogar@amazon.com>
2025-08-13 14:55:27 +0000

    Fix Out-of-Band changes to service_connect_configuration are not detected

--- internal/service/ecs/service_test.go
+++ internal/service/ecs/service_test.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/YakDriver/regexache"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ecs"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/ecs/types"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/retry"
 	"github.com/hashicorp/terraform-plugin-testing/compare"
@@ -2260,6 +2261,37 @@ func TestAccECSService_ServiceConnect_remove(t *testing.T) {
 	})
 }
 
+// Regression for https://github.com/hashicorp/terraform-provider-aws/issues/42818
+func TestAccECSService_ServiceConnect_outOfBandRemoval(t *testing.T) {
+	ctx := acctest.Context(t)
+	var service awstypes.Service
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	resourceName := "aws_ecs_service.test"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:                 func() { acctest.PreCheck(ctx, t) },
+		ErrorCheck:               acctest.ErrorCheck(t, names.ECSServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccServiceConfig_serviceConnectBasic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckServiceExists(ctx, resourceName, &service),
+					testAccCheckServiceDisableServiceConnect(ctx, &service),
+				),
+				ExpectNonEmptyPlan: true,
+			},
+			{
+				Config: testAccServiceConfig_serviceConnectBasic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckServiceExists(ctx, resourceName, &service),
+					resource.TestCheckResourceAttr(resourceName, "service_connect_configuration.0.enabled", acctest.CtTrue),
+				),
+			},
+		},
+	})
+}
+
 func TestAccECSService_Tags_basic(t *testing.T) {
 	ctx := acctest.Context(t)
 	var service awstypes.Service
@@ -2485,9 +2517,10 @@ func testAccCheckServiceExists(ctx context.Context, name string, service *awstyp
 
 		conn := acctest.Provider.Meta().(*conns.AWSClient).ECSClient(ctx)
 
+		var output *awstypes.Service
 		err := retry.RetryContext(ctx, 1*time.Minute, func() *retry.RetryError {
 			var err error
-			service, err = tfecs.FindServiceNoTagsByTwoPartKey(ctx, conn, rs.Primary.ID, rs.Primary.Attributes["cluster"])
+			output, err = tfecs.FindServiceNoTagsByTwoPartKey(ctx, conn, rs.Primary.ID, rs.Primary.Attributes["cluster"])
 
 			if tfresource.NotFound(err) {
 				return retry.RetryableError(err)
@@ -2500,6 +2533,29 @@ func testAccCheckServiceExists(ctx context.Context, name string, service *awstyp
 			return nil
 		})
 
+		if err != nil {
+			return err
+		}
+
+		*service = *output
+
+		return nil
+	}
+}
+
+func testAccCheckServiceDisableServiceConnect(ctx context.Context, service *awstypes.Service) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		conn := acctest.Provider.Meta().(*conns.AWSClient).ECSClient(ctx)
+
+		input := &ecs.UpdateServiceInput{
+			Cluster: service.ClusterArn,
+			Service: service.ServiceName,
+			ServiceConnectConfiguration: &awstypes.ServiceConnectConfiguration{
+				Enabled: false,
+			},
+		}
+
+		_, err := conn.UpdateService(ctx, input)
 		return err
 	}
 }
@@ -7110,6 +7166,40 @@ resource "aws_ecs_service" "test" {
   desired_count   = 1
 }
 `, rName)
+}
+
+func testAccServiceConfig_serviceConnect_missingNamespace(rName, clusterName string) string {
+	return fmt.Sprintf(`
+resource "aws_ecs_cluster" "test" {
+  name = %[2]q
+}
+
+resource "aws_ecs_task_definition" "test" {
+  family = %[2]q
+
+  container_definitions = <<DEFINITION
+[
+  {
+    "cpu": 128,
+    "essential": true,
+    "image": "mongo:latest",
+    "memory": 128,
+    "name": "mongodb"
+  }
+]
+DEFINITION
+}
+
+resource "aws_ecs_service" "test" {
+  name            = %[1]q
+  cluster         = aws_ecs_cluster.test.id
+  task_definition = aws_ecs_task_definition.test.arn
+  desired_count   = 1
+  service_connect_configuration {
+    enabled   = true
+  }
+}
+`, rName, clusterName)
 }
 
 func testAccServiceConfig_availabilityZoneRebalancing(rName string, serviceRebalancing string) string {
